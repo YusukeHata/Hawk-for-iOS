c9af16b1550f17261347aa903b8fb241695a5725
YusukeHata <yusuke.hata.dev@gmail.com>
2017-06-17 15:05:01 +0900

    Add project

---
+++ Hawk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hawk
+//
+//  Created by 畑　祐輔　 on 2017/06/17.
+//  Copyright © 2017年 YusukeHata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
